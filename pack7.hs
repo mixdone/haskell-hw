cdd70dcc95efe03ebe0ee5ebfcbe4ac85cf4782b
mixdone <m.diza@mail.ru>
2022-12-20 23:42:28 +0700

    исправление

--- pack7.hs
+++ pack7.hs
@@ -1,5 +1,7 @@
-import System.IO  
+import System.IO
 import System.Directory
+import Control.DeepSeq
+import Control.Exception
 import Data.Text
 import qualified Data.ByteString.Lazy as B
 -- Pack 7. IO
@@ -13,15 +15,17 @@ printFile fileToPrint = do
   hClose handle
 
 -- task 2 Text Check
-areEqualText :: FilePath -> FilePath -> IO (Bool)
+areEqualText :: FilePath -> FilePath -> IO Bool
 areEqualText file1 file2 = do
   f1 <- openFile file1 ReadMode
   f2 <- openFile file2 ReadMode
   c1 <- hGetContents f1
   c2 <- hGetContents f2
+  x1 <- evaluate $ force c1
+  x2 <- evaluate $ force c2
   hClose f1
   hClose f2
-  return (c1 == c2)
+  return (x1 == x2)
 
 -- task 3 Dos2Unix
 dos2unix :: String -> IO ()
@@ -50,9 +54,11 @@ areEqualBin file1 file2 = do
   f2 <- openBinaryFile file2 ReadMode
   c1 <- B.hGetContents f1
   c2 <- B.hGetContents f2
+  x1 <- evaluate $ force c1
+  x2 <- evaluate $ force c2
   hClose f1
   hClose f2
-  return (c1 == c2)
+  return (x1 == x2)
 
 -- task 5 No Vimmers?
 fileIsBeingEdited :: FilePath -> IO (Bool)
